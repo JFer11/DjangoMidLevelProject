88c7d10aa6913059339c930f0dbdfac072113a34
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-05 12:20:19 -0300

    project initialization

---
+++ businessweb/core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+
+
+def home(request):
+    return HttpResponse("Home")
+
+
+def about(request):
+    return HttpResponse("About")
+
+
+def services(request):
+    return HttpResponse("Services")
+
+
+def store(request):
+    return HttpResponse("Store")
+
+
+def contact(request):
+    return HttpResponse("Contact")
+
+
+def blog(request):
+    return HttpResponse("Blog")
+
+
+def sample(request):
+    return HttpResponse("Sample")
