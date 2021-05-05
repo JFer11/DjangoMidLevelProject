a2b0a37935b73dbf6d662c365f15eb4e865ac8b5
Joaco <jnfernandez.ojeda@gmail.com>
2021-05-05 14:35:06 -0300

    Service App Added (Dynamic Content)
    Added dynamic content which will be shown through service.html template. In the admin page, an admin can create instances of the model Service, and said model will be displayed in the service page dynamically. In other word, when we create or delete new Services in the admin page, these changes will be reflected in the public website. The model has serveral fields, for instance, an ImageField (this requires a python package called Pillow).

--- businessweb/businessweb/urls.py
+++ businessweb/businessweb/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from businessweb import settings
 
 
 urlpatterns = [
     # Core Paths
     path('core/', include('core.urls')),
+    # Services Paths
+    path('services/', include('services.urls')),
 
     # Admin Paths
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
